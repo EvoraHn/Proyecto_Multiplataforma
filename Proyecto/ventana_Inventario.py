3326396ebbd1a85e67bf5f06caf261277a7cfc34
Saudyz <saudyzavala123@gmail.com>
2020-03-30 21:53:15 -0600

    Diseño de venta de stock

--- Proyecto/ventana_Inventario.py
+++ Proyecto/ventana_Inventario.py
@@ -32,14 +32,20 @@ class Main(QWidget):
 
     def main_desing(self):
         """ Diseño principal de la aplicación. """
+        self.label_buscar = QLabel("Buscar: ")
+        self.input_buscar = QLineEdit()
         self.inventario_list = QListWidget()
-        self.btn_new = QPushButton("Agregar")
+        self.btn_agregar = QPushButton("Age")
+        self.btn_editar = QPushButton("E")
+        self.btn_eliminar = QPushButton("E")
+        self.btn_buscar = QPushButton("B")
         self.label_id = QLabel("ID Categoria: ")
         self.input_id = QLineEdit()
         self.label_name = QLabel("Nombre: ")
         self.input_name = QLineEdit()
         self.label_descripcion = QLabel("Descripcion: ")
         self.input_descripcion = QLineEdit()
+        self.label_vacia = QLabel(" ")
 
     def layouts(self):
         """ Layouts que componen la aplicación. """
@@ -47,25 +53,34 @@ class Main(QWidget):
         self.main_layout = QHBoxLayout()
         self.left_layout = QFormLayout()
         self.right_main_layout = QVBoxLayout()
-        self.right_top_layout = QHBoxLayout()
+        self.right_top_layout =  QFormLayout()
         self.right_bottom_layout = QHBoxLayout()
         self.top_layout = QVBoxLayout()
-        self.left_bottom_layout = QFormLayout()
+        self.left_bottom_layout = QHBoxLayout()
 
         # Agregar los layouts hijos al layout padre
         self.right_main_layout.addLayout(self.right_top_layout)
         self.right_main_layout.addLayout(self.right_bottom_layout)
-        self.main_layout.addLayout(self.left_layout, 40)
-        self.main_layout.addLayout(self.right_main_layout, 60)
+        self.main_layout.addLayout(self.left_layout)
+        self.main_layout.addLayout(self.right_main_layout)
         self.main_layout.addLayout(self.top_layout)
         self.main_layout.addLayout(self.left_bottom_layout)
 
         # Agregar widgets a los layouts
-        self.right_top_layout.addWidget(self.inventario_list)
-        self.right_bottom_layout.addWidget(self.btn_new)
-        self.left_bottom_layout.addRow(self.label_id, self.input_id)
-        self.left_bottom_layout.addRow(self.label_name, self.input_name)
-        self.left_bottom_layout.addRow(self.label_descripcion, self.input_descripcion)
+        self.left_bottom_layout.addWidget(self.inventario_list)
+        self.right_bottom_layout.addWidget(self.btn_agregar)
+        self.right_bottom_layout.addWidget(self.btn_editar)
+        self.right_bottom_layout.addWidget(self.btn_eliminar)
+        #self.bottom_layout.addRow("", self.btn_agregarProducto, self. btn_editarProducto, self.btn_eliminarProducto)
+        self.right_top_layout.addRow(self.label_buscar, self.input_buscar)
+        self.right_top_layout.addRow("", self.btn_buscar)
+        self.right_top_layout.addRow("", self.label_vacia)
+        self.right_top_layout.addRow("", self.label_vacia)
+        self.right_top_layout.addRow("", self.label_vacia)
+        self.right_top_layout.addRow(self.label_id, self.input_id)
+        self.right_top_layout.addRow(self.label_name, self.input_name)
+        self.right_top_layout.addRow(self.label_descripcion, self.input_descripcion)
+        #self.right_top_layout.addRow(self.btn_agregar, self.btn_editar, self.btn_eliminar)
 
 
         # Colocar el layout principal en la ventana principal
