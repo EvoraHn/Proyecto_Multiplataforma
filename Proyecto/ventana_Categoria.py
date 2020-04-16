09fb4e93db5ab34579c989fcdd1c9c12ef28a1eb
Fernando Martinez <mfernandomartinez1@yahoo.com>
2020-04-16 10:43:26 +0200

    Base y Crud Categoria

--- Proyecto/ventana_Categoria.py
+++ Proyecto/ventana_Categoria.py
@@ -20,6 +20,9 @@ class Main(QWidget):
 
     def __init__(self):
         super().__init__()
+
+        self.producto_db = ProductoDB("laminas.db")
+
         self.setGeometry(450, 150, 750, 600)
         self.UI()
         self.show()
@@ -35,16 +38,25 @@ class Main(QWidget):
         self.label_buscar = QLabel("Buscar: ")
         self.input_buscar = QLineEdit()
         self.inventario_list = QListWidget()
+        #self.inventario_list.connect(self.Mostrar_producto)
+
         self.btn_agregar = QPushButton("Agregar")
+        self.btn_agregar.clicked.connect(self.insert)
+
         self.btn_editar = QPushButton("Editar")
         self.btn_eliminar = QPushButton("Eliminar")
+
         self.btn_buscar = QPushButton("BUSCAR")
+
         self.label_idCategoria = QLabel("ID Categoria: ")
         self.input_idCategoria = QLineEdit()
+
         self.label_tipoCategoria = QLabel("Tipo Categoria: ")
         self.input_tipoCategoria = QLineEdit()
+
         self.label_descripcion = QLabel("Descripcion: ")
         self.input_descripcion = QLineEdit()
+
         self.label_vacia = QLabel(" ")
 
     def layouts(self):
@@ -69,6 +81,7 @@ class Main(QWidget):
         # Agregar widgets a los layouts
         self.left_bottom_layout.addWidget(self.inventario_list)
         self.right_bottom_layout.addWidget(self.btn_agregar)
+
         self.right_bottom_layout.addWidget(self.btn_editar)
         self.right_bottom_layout.addWidget(self.btn_eliminar)
         #self.bottom_layout.addRow("", self.btn_agregarProducto, self. btn_editarProducto, self.btn_eliminarProducto)
@@ -86,8 +99,140 @@ class Main(QWidget):
         # Colocar el layout principal en la ventana principal
         self.setLayout(self.main_layout)
 
-   
+    
+    def insert(self):
+        """ Insertar los valores del formulario a la tabla de producto """
+        # Verificar si los valores requeridos fueron agregados
+        if (self.input_tipoCategoria.text() or
+                self.input_descripcion.text() !=""):
+            categoria = (self.input_tipoCategoria.text(),self.input_descripcion.text())
+                        
+            try:
+                self.producto_db.add_categoria(categoria)
+                QMessageBox.information(
+                    self, "Información", "su categoria se agreg correctamente")
+                self.inventario_list.clear()
+                self.set_categoria_list()
+                self.limpiar()
+                #self.close()
+                #self.main = Main()
+            except Error as e:
+                QMessageBox.information(
+                    self, "Error", "Error al momento de agregar el producto")
+        else:
+            QMessageBox.information(
+                self, "Advertencia", "Debes ingresar toda la información")
 
+    def set_categoria_list(self):
+        """ Obtiene las tuplas de Productos y las muestra en la lista """
+        productos = self.producto_db.get_all_categoria()
+
+        if productos:
+            for  categoria in productos:
+                self.inventario_list.addItem(
+                    "{0} -- {1} -- {2}".format(categoria[1], categoria[2],categoria[2]))
+     
+     
+    def Mostrar_categoria(self):
+        """ Muestra los atributos del producto que se encuentra seleccionado """
+        categoria = self.inventario_list.currentItem().text()
+        id = categoria.split(" --- ")[0]
+
+        producto = self.producto_db.Obtener_Producto(id)
+        
+        if producto:
+            #se deshabilitan los textbox
+            self.Bloquear_Inputs(True)
+            idCategoria =  categoria[0]
+            tipoCategoria =  categoria[1]
+            descripcion =  categoria[2]
+            #se muestran los valores en los text
+            self.input_idCategoria.setText((str(idCategoria)))
+            self.input_nombre.setText((str(tipoCategoria)))
+            self.input_descripcion.setText((str(descripcion)))
+
+    def limpiar(self):
+        self.input_idCategoria.setText("")
+        self.input_tipoCategoria.setText("")
+        self.input_descripcion.setText("")
+
+class ProductoDB:
+    """ Base de datos SQLite para los productos. """
+
+    def __init__(self, db_filename):
+        """ Inicializador de la clase """
+        self.connection = self.create_connection(db_filename)
+        self.producto_query = """ CREATE TABLE IF NOT EXISTS categoria (
+                                    idCategoria integ unique primary key,
+                                    NombreCategoria TEXT,
+                                    Descripcion TEXT
+                                  );
+                                """
+        self.create_table(self.connection, self.producto_query)
+    
+    
+    def create_connection(self, db_filename):
+        """ Crear una conexión a la base de datos SQLite """
+        conn = None
+
+        # Tratar de conectarse con SQLite y crear la base de datos
+        try:
+            conn = sqlite3.connect(db_filename)
+            print("Conexión realizada. Versión {}".format(sqlite3.version))
+        except Error as e:
+            print(e)
+        finally:
+            return conn
+
+    def create_table(self, conn, query):
+        """
+        Crea una tabla basado en los valores de query.
+        :param conn: Conexión con la base de datos.
+        :param query: La instrucción CREATE TABLE.
+        :return:
+        """
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query)
+        except Error as e:
+            print(e)
+
+    def add_categoria(self, categoria):
+        """
+        Realiza una inserción a la tabla de categoria.
+        :param producto: Una estructura que contiene
+                         los datos de categoria.
+        :return:
+        """
+        sqlInsert = """
+                    INSERT INTO categoria(
+                         idCategoria, NombreCategoria, Descripcion)
+                     VALUES(?, ?)
+                    """
+
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(sqlInsert, categoria)
+            # Indicarle al motor de base de datos
+            # que los cambios sean persistentes
+            self.connection.commit()
+        except Error as e:
+            print(e)
+    
+    
+    def get_all_categoria(self):
+        """ Obtiene todas las tuplas de la tabla categoria """
+        sqlQuery = " SELECT * FROM categoria ORDER BY ROWID ASC "
+
+        try:
+            cursor = self.connection.cursor()
+            categoria = cursor.execute(sqlQuery).fetchall()
+
+            return categoria
+        except Error as e:
+            print(e)
+
+        return None
 
 
 def main():
